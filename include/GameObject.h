6d1c409a0026277b7f24f0d23080453981af68ec
Jacob R <fonfalleh@gmail.com>
2013-09-01 13:56:08 +0200

    Small changes
    Mostly been thinking about structure.
    Will probably not follow MVC as originally planned

---
+++ include/GameObject.h
@@ -0,0 +1,15 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+
+class GameObject
+{
+    public:
+        GameObject();
+        virtual ~GameObject();
+        virtual update();
+    protected:
+    private:
+};
+
+#endif // GAMEOBJECT_H
---
+++ include/Scene.h
@@ -0,0 +1,15 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "GameObject.h"
+
+class Scene
+{
+    public:
+        Scene();
+        virtual ~Scene();
+    protected:
+    private:
+};
+
+#endif // SCENE_H
